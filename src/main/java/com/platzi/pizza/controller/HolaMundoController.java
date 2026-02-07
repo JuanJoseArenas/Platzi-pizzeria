517ad8b4123236b651408dbc029b22675b3f9967
Juan Jose Arenas <juanjoseaq99@gmail.com>
2026-02-07 12:55:56 -0500

    Proyecto Pizzeria

---
+++ src/main/java/com/platzi/pizza/controller/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.platzi.pizza.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Saludar")
+public class HolaMundoController {
+
+    @GetMapping("juanjo")
+    public String saludar(){
+        return "Hola Juanjo";
+    }
+}
